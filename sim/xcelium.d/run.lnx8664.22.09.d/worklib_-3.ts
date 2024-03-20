c8eaf63726d0891632b962aa77523ff0435f58d1
teddywhy <teddywhy@gmail.com>
2024-03-20 12:47:12 +0000

    clear useless file

--- sim/xcelium.d/run.lnx8664.22.09.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1663175564 /edatools/cadence/XCELIUM-22.09.010/tools.lnx86/methodology/UVM/CDNS-1.2/sv/src/uvm_pkg.sv
-1663175561 /edatools/cadence/XCELIUM-22.09.010/tools.lnx86/methodology/UVM/CDNS-1.2/additions/sv/cdns_uvm_pkg.sv
